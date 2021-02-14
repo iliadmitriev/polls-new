fb625c5c77e839717606b382289502878987b062
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-15 01:14:32 +0300

    output request data

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.views.generic import View
 from django.utils.translation import gettext as _
+from pprint import pprint, pformat
 
 
 class IndexView(View):
     def get(self, *args, **kwargs):
-        # request = self.request
-        output = _('hello world')
-        response = HttpResponse(output)
+        request = self.request
+        output = _('hello world') + '\n%s' % pformat(request.__dict__)
+
+        response = HttpResponse(output, content_type='text/plain; charset=utf-8')
         return response
 
