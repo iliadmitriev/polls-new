2282d5fabe6f0d3544bc8af93f44455703c4ed05
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-15 18:00:32 +0300

    * result page * vote view with redirect

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from polls.views import PollsIndexView, PollsDetailView
+from polls.views import (
+    PollsIndexView,
+    PollsDetailView,
+    PollsResultView,
+    PollsVoteView
+)
 
 urlpatterns = [
     path('', PollsIndexView.as_view(), name='polls-index'),
-    path('<int:pk>/', PollsDetailView.as_view(), name='polls-detail')
+    path('<int:pk>/', PollsDetailView.as_view(), name='polls-detail'),
+    path('<int:pk>/result', PollsResultView.as_view(), name='polls-result'),
+    path('<int:pk>/vote', PollsVoteView.as_view(), name='polls-vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, View
+from django.shortcuts import HttpResponseRedirect, reverse
 from .models import Question
 
 
@@ -10,3 +11,14 @@ class PollsIndexView(ListView):
 class PollsDetailView(DetailView):
     model = Question
     template_name = 'polls_detail_view.html'
+
+
+class PollsResultView(DetailView):
+    model = Question
+    template_name = 'polls_results_view.html'
+
+
+class PollsVoteView(View):
+    def post(self, request, *args, **kwargs):
+        redirect = HttpResponseRedirect(reverse('polls-result', kwargs=kwargs))
+        return redirect
