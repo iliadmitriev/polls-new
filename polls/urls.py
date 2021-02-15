07becd99887c1483ada7ddc1534fe9c38fdb5c7e
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-15 16:06:07 +0300

    back link

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from polls.views import PollsIndexView, PollsDetailView
 
 urlpatterns = [
-    path('', PollsIndexView.as_view(), name='index'),
+    path('', PollsIndexView.as_view(), name='polls-index'),
     path('<int:pk>/', PollsDetailView.as_view(), name='polls-detail')
 ]
