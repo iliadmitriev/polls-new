ddd890fb6da90d78e74046c00cc09c8437befb77
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-15 12:34:04 +0300

    index view tune and detail view

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import View, DetailView
 from django.utils.translation import gettext as _
 from pprint import pprint, pformat
+from .models import Question, Choice
 
 
-class IndexView(View):
+class PollsIndexView(View):
     def get(self, *args, **kwargs):
         request = self.request
         output = _('hello world') + '\n%s' % pformat(request.__dict__)
@@ -12,3 +13,7 @@ class IndexView(View):
         response = HttpResponse(output, content_type='text/plain; charset=utf-8')
         return response
 
+
+class PollsDetailView(DetailView):
+    model = Question
+    template_name = 'polls_detail_view.html'
