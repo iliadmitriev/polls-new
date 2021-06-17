f722b714a862858c2dcb564caaf5fa4977da2e20
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-06-17 17:27:18 +0300

    test new feature

--- polls/views.py
+++ polls/views.py
@@ -27,7 +27,10 @@ class PollsResultView(DetailView):
 class PollsVoteView(View):
     def post(self, request, *args, **kwargs):
         question = get_object_or_404(Question, pk=kwargs.get('pk'))
+        ABC = None
         try:
+            if (ABC):
+                print("None")
             choice_voted = question.choice_set.get(pk=request.POST['choice'])
         except (KeyError, Choice.DoesNotExist):
             return HttpResponseRedirect(reverse('polls-detail', kwargs=kwargs))
